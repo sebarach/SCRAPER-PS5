4c75d3c9315bd66d76e271365e4eb2cabd52f2e5
Sebastian <sebastian.sc1990@gmail.com>
2022-06-29 16:49:31 -0400

    updating.

--- scraping.js
+++ scraping.js
@@ -6,8 +6,36 @@ const urlWeplay = 'https://www.weplay.cl/consola-playstation-5.html';
 const urlLaPolar = 'https://www.lapolar.cl/consola-sony-playstation-5-control/23395401.html';
 const urlFalabella = 'https://www.falabella.com/falabella-cl/product/15706659/Consola-Sony-PS5-Digital/15706659';
 const urlHites = 'https://www.hites.com/consola-sony-playstation-5-edicion-con-disco-837800001.html?gclid=Cj0KCQjw8O-VBhCpARIsACMvVLMYOVflwDsi5fPx4jWSU-7SarW_AqMnvNg-yxQKPrSnrlLmzkmJKYsaAqfEEALw_wcB';
-
+const urlPcNitro = 'https://pcnitro.cl/inicio/19241-playstation-5-consola-playstation-5-sony-825gb-digital-edition-color-blanco-y-negro.html';
+const urlGoldenGamer = 'https://goldengamers.cl/products/playstation-5-digital-edicion-jp';
 const datos = [];
+
+
+
+async function scrapearGoldenGamerPS5() {
+    const { data } = await axios.get(urlGoldenGamer);
+    const $ = cheerio.load(data);
+    const precio = $('#ProductPrice-product-template > span').first().text();
+    const PRECIOREAL = parseInt(precio.replace(/[^0-9,.]+/g, "").replace(/[,.]+/g, ""));
+    datos.push({ url: urlGoldenGamer, precio: precio, precioParse: PRECIOREAL });
+}
+
+
+async function scrapearPcNitroPS5() {
+    const browser = await puppeteer.launch({
+        defaultViewport: null
+    });
+    const page = await browser.newPage();
+    await page.goto(urlPcNitro);
+    const text = await page.evaluate(() => {
+        return document.querySelector('#main > div.tvproduct-page-wrapper > div.tvprduct-image-info-wrapper.clearfix.row > div.col-md-6.tv-product-page-content > div.product-prices > div.product-price.h5 > div > span').innerText;
+    });
+    await page.close();
+    await browser.close();
+
+    const PRECIOREAL = parseInt(text.replace(/[^0-9,.]+/g, "").replace(/[,.]+/g, ""));
+    datos.push({ url: urlPcNitro, precio: text, precioParse: PRECIOREAL });
+}
 
 async function scrapearHitesPS5() {
     const { data } = await axios.get(urlHites);
@@ -58,23 +86,29 @@ async function scrapearFalabellaPS5() {
     datos.push({ url: urlFalabella, precio: text, precioParse: PRECIOREAL });
 }
 
-const allPromise = Promise.all([scrapearParisPS5(), scrapearWeplayPS5(),scrapearLaPolarPS5(),scrapearFalabellaPS5(),scrapearHitesPS5()]);
+const allPromise = Promise.all([scrapearParisPS5(), scrapearWeplayPS5(),scrapearLaPolarPS5(),scrapearFalabellaPS5(),scrapearHitesPS5(),scrapearPcNitroPS5(),
+    scrapearGoldenGamerPS5()]);
 
-allPromise.then(values => {
-   // console.log(datos);
+setTimeout(() => {
+    response();
+},30000);
+
+
+const response = () =>allPromise.then(values => {
     DrawTable(datos);
   }).catch(error => {
     error;
   });
 
 
+
 function DrawTable(objet)
 {
-    console.log("##############  Sistema de Rastreo para PlayStation 5 SEBITA  ##############")
+    console.log("##############  Rastreo para PlayStation 5 ##############")
     console.log("-----------------------------------------------------------------------")
     console.log("-----------------------------------------------------------------------")
     objet.map((values)=>{
-        console.log(values.precioParse < 600000 ? 'En Oferta' : 'Sin Oferta')  
+        console.log(values.precioParse < 600000 ? '@@@@@@ OFERTA @@@@@@@' : 'Sin Oferta')  
         console.log(`Precio : $${values.precioParse} Pesos`)   
         console.log(`Link del Comercio : $ ${values.url}`)
         console.log("****************************************************")     
