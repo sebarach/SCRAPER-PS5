0bc66ede3bb12454bb1d55505b9a16e322017fdb
Sebastian <sebastian.sc1990@gmail.com>
2022-06-30 17:12:09 -0400

    asd

--- scraping.js
+++ scraping.js
@@ -56,10 +56,15 @@ async function scrapearRipleyPS5() {
 
 async function scrapearTottusPS5() {
     try {
-    const browser = await puppeteer.launch({
-        defaultViewport: null,headless:true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox","--single-process"]
-    });
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+              '--disable-dev-shm-usage',
+              '--single-process'
+            ],
+          });
     const page = await browser.newPage();
     await page.goto(urlTottus);
     const text = await page.evaluate(() => {
